bcd6190844c8785a038d438c3cae9f9d9ea8004c
Doğan Alıcı <doganalici2144@gmail.com>
2021-06-21 19:56:02 +0300

    21 Haziran

---
+++ OOP1/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{       
+        // BU TİP KLASLARDA SADECE ÖZELLİK OLUR !
+    class Product  // ÜRÜN  
+    {
+        public int Id { get; set; } // NESNEYİ DİĞERİNDEN AYIRT ETMEK İÇİN VERİTABANINDA KULLANILAN ALAN
+        public int CategoryId { get; set; } // ÜRÜNÜN HANGİ KATEGORİYE AİT OLDUĞU
+        public string ProductName { get; set; } // ÜRÜNÜN ADI
+        public double UnitPrice { get; set; } // ÜRÜNÜN FİYATI
+        public int UnitInStock { get; set; } // STOK ADEDİ
+    }
+}
