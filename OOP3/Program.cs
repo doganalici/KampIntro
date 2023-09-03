ffa46095a5a3323b0bbdbd58fd09e3069b7ec8fb
Doğan Alıcı <doganalici2144@gmail.com>
2023-09-03 19:31:58 +0300

    5.Gün

---
+++ OOP3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> { smsLoggerService, fileLoggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+
+            basvuruManager.BasvuruYap(esnafKrediManager, loggers);
+            //basvuruManager.BasvuruYap(tasitKrediManager);
+            //basvuruManager.BasvuruYap(konutKrediManager);
+            Console.WriteLine("\n\n\n");
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, tasitKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+            Console.ReadKey();
+        }
+    }
+}
